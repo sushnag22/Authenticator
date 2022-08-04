a272f725b297abc4609e7b3b01ea0c7e9262645e
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-08-04 20:02:41 +0530

    Add DashboardActivity.java class and activity_dashboard.xml

---
+++ app/src/main/java/tech/sushnag22/authenticator/DashboardActivity.java
@@ -0,0 +1,19 @@
+package tech.sushnag22.authenticator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        String welcomeText = "Welcome " + getIntent().getStringExtra("username") + "!";
+        TextView tvWelcome = findViewById(R.id.tvWelcome);
+        tvWelcome.setText(welcomeText);
+    }
+}
