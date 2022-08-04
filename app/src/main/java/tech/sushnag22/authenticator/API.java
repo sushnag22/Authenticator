eeff8fb0825e41b5c1754c0c935a03f59f20d4a3
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-08-04 20:00:54 +0530

    Add RetrofitClient.java class and API.java interface for consuming the REST API

---
+++ app/src/main/java/tech/sushnag22/authenticator/API.java
@@ -0,0 +1,21 @@
+package tech.sushnag22.authenticator;
+
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface API {
+
+    @POST("register")
+    Call<ResponseBody> createUser (
+            @Body User user
+    );
+
+    @POST("login")
+    Call<ResponseBody> checkUser (
+            @Body User user
+    );
+
+}
