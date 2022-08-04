e0624c82d96998cc3465c65c0002292851deead8
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-08-04 19:57:16 +0530

    Add model User.java class

---
+++ app/src/main/java/tech/sushnag22/authenticator/User.java
@@ -0,0 +1,11 @@
+package tech.sushnag22.authenticator;
+
+public class User {
+    private String userName;
+    private String password;
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+}
