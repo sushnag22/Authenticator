a3e0df8920740bb062ebbd83a467c58017c7ce27
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-08-06 13:24:54 +0530

    Update base URL of REST API in Retrofit client

--- app/src/main/java/tech/sushnag22/authenticator/RetrofitClient.java
+++ app/src/main/java/tech/sushnag22/authenticator/RetrofitClient.java
@@ -1,11 +1,10 @@
 package tech.sushnag22.authenticator;
 
-
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static  final String BASE_URL = "http://ipAddress:8080/api/";
+    private static  final String BASE_URL = "https://authenticator-rest-api.herokuapp.com/api/";
     private static RetrofitClient mInstance;
     private Retrofit retrofit;
 
